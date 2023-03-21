80a60766cfa395cbda9d870cc0dfc70cedbf7df4
ianjhill <ianjhill1@gmail.com>
2023-03-21 16:16:32 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ian Hill on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
